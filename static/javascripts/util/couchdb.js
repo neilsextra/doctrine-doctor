09bbde8b89984e714577d522678fad39df3553ed
thesourorange <thesourorange@outlook.com>
2023-09-27 00:03:53 +0000

    Added link documents

--- static/javascripts/util/couchdb.js
+++ static/javascripts/util/couchdb.js
@@ -238,6 +238,42 @@ CouchDB.prototype.get = function (corpus, id) {
     
 }
 
+CouchDB.prototype.retrieveLinks = function (corpus, id) {
+
+    return new Promise((accept, reject) => {
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.open("GET",
+            `/retrieve/links/${corpus}?couchdb-url=${encodeURIComponent(this.__url)}&${corpus}-id=${encodeURIComponent(id)}`, true);
+
+        xhttp.onreadystatechange = async function () {
+
+            if (this.readyState === 4 && this.status === 200) {
+                var paths = [];
+                var response = JSON.parse(this.responseText);
+
+                accept({
+                    status: this.status,
+                    response: response
+                });
+
+            } else if (this.status === 500) {
+
+                reject({
+                    status: this.status,
+                    message: this.statusText
+                });
+
+            }
+
+        };
+
+        xhttp.send();
+
+    });
+    
+}
+
 CouchDB.prototype.getDocument = function (documentId) {
 
     return this.get("document", documentId)
